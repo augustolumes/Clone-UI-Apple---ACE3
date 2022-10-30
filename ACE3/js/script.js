fc1ff456214904765eddb50aff5dfab315e1a824
augustolumes <82983177+augustolumes@users.noreply.github.com>
2022-10-30 16:02:08 -0300

    Delete ACE3 directory

--- ACE3/js/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function (){
-    $(".hamburguer").click(function (){
-        $(this).toggleClass("hamburguer-active");
-        //Toggle do Menu, final da aulda
-        $(".nav-list-mobile").toggleClass("active");
-    });
-});
